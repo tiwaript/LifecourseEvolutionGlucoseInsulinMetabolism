2535e908981732fe55b507d6cccc143c18d9d62d
tiwaript <pradeep.tiwari@igib.in>
2021-04-05 02:36:32 +0530

    Data Preprocessing code

---
+++ R_scripts/Data_preprocessing.R
@@ -0,0 +1,9 @@
+# Packages Required 
+require(foreign)
+require(tidyr)
+# data import 
+dat_ihoma<-read.spss("./Data/PMNS PREDIABETES DATA_27JUNE2019.sav",
+                        to.data.frame = T,use.value.labels = T)
+
+dat_PMNS<-read.spss("./Data/pmns original.sav",to.data.frame = T,use.value.labels = T)
+# 
